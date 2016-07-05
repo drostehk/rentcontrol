cc8bd662756ee3065840b4f22a49d4f47633d2b2
Natalie Lung <lovya.natlung@gmail.com>
2016-07-05 16:21:29 +0800

    Layout update
    stackable grid and set default values to web form

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-// $('.ui.dropdown')
-//   .dropdown()
-// ;
+$(function() {
+	$('.dropdown').dropdown({ onChange: function(value){console.log(value);}});
+});
 
-//   $('#range-3').range({
-//     min: 0,
-//     max: 5,
-//     start: 1,
-//     onChange: function(value) {
-//       /**var cats = ['< 40','40 - 69.9','70-99.9','100 - 159.9','> 160']**/
-//       var cats = ['A', 'B', 'C', 'D', 'E']
-//       $('#display-3').html(cats[value]);
-//     }
-//   });
+$(document).ready(function() {
 
-//   $('.menu .item')
-//   .tab()
-// ;
+	var now = new Date();
+	if (now.getMonth() == 12) {
+	    var current = new Date(now.getFullYear() + 1, 0, 1);
+	} else {
+	    var current = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate());
+	}
 
+	document.getElementById("duedate").value = current.toLocaleString();
 
-// $('#duedate').calendar();
-
-// var now = new Date();
-// if (now.getMonth() == 11) {
-//     var current = new Date(now.getFullYear() + 1, 0, 1);
-// } else {
-//     var current = new Date(now.getFullYear(), now.getMonth() + 1, now.getDate());
-// }
-
-// document.getElementsByTagName("INPUT")[0].value = current;
-
-$(function(){
-  $('.dropdown').dropdown({ onChange: function(value, text){console.log(value, text);}});
+	$('input[name="duedate"]').daterangepicker({
+        singleDatePicker: true,
+        showDropdowns: true
+    });
 
 });
